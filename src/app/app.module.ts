95f52fe13b45fb8f1a18f6f254cdf03cdb18368c
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2022-09-15 11:24:18 -0400

    feat: create get method on service books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/books/store/books.selector.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Books.Selector } from './books.selector';
-
-describe('Books.Selector', () => {
-  it('should create an instance', () => {
-    expect(new Books.Selector()).toBeTruthy();
-  });
-});
