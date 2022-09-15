e9f255bab6515d0a6c500cd67a65f82aa9337bb9
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2022-09-15 15:08:42 -0400

    feat: Implements action, effect and reducer methods

--- src/app/books/store/books.action.ts
+++ src/app/books/store/books.action.ts
@@ -1,2 +1,13 @@
-export class BooksAction {
-}
+import { createAction, props } from "@ngrx/store";
+import { Books } from "./books";
+
+// Invocar uma chamada de API
+export const invokeBooksAPI = createAction(
+  '[Books API] Invoke Books Fetch API'
+);
+
+// Enviando parametros atraves de "props", essa action salva a response da api na Store
+export const booksFetchAPISuccess = createAction(
+  '[Books API] Fetch API Success',
+  props<{ allBooks: Books[] }>()
+);
