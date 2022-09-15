cd45b2661a7da429dee8570ca363ccdcef06aad1
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2022-09-15 18:02:38 -0400

    feat: Implement select book by id selector

--- src/app/books/store/books.selector.ts
+++ src/app/books/store/books.selector.ts
@@ -1,7 +1,18 @@
-import { createFeatureSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { Books } from "./books";
 
 /**
  * Usado para buscar todos os dados do modulo de recursos
  */
 export const selectBooks = createFeatureSelector<Books[]>('mybooks');
+
+export const selectBookById = (bookId: number) =>
+  // Here 'createSelector' is used to fetch the slice of data from the ngrx store.
+  createSelector(selectBooks, (books: Books[]) => {
+    var bookById = books.filter((_) => _.id == bookId);
+    if (bookById.length == 0) {
+      return null;
+    }
+
+    return bookById[0];
+  });
